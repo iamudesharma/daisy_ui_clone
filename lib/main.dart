4b5177fb122f8281a41eae4afb69913b1f96c445
iamudesharma <udesh2568@gmail.com>
2023-08-11 22:38:22 +0530

    wasm added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:daisy_ui_clone/components/card_page.dart';
-import 'package:flex_color_scheme/flex_color_scheme.dart';
+// import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 import 'components/alert_page.dart';
@@ -20,37 +20,37 @@ class MyApp extends StatelessWidget {
 // same or higher package version, but still same major version. If you
 // use a lower package version, some properties may not be supported.
 // In that case remove them after copying this theme to your app.
-      theme: FlexThemeData.light(
-        scheme: FlexScheme.materialBaseline,
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 7,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 10,
-          blendOnColors: false,
-          useTextTheme: true,
-          useM2StyleDividerInM3: true,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        swapLegacyOnMaterial3: true,
-        // To use the Playground font, add GoogleFonts package and uncomment
-        // fontFamily: GoogleFonts.notoSans().fontFamily,
-      ),
-      darkTheme: FlexThemeData.dark(
-        scheme: FlexScheme.materialBaseline,
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 13,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 20,
-          useTextTheme: true,
-          useM2StyleDividerInM3: true,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        swapLegacyOnMaterial3: true,
-        // To use the Playground font, add GoogleFonts package and uncomment
-        // fontFamily: GoogleFonts.notoSans().fontFamily,
-      ),
+      // theme: FlexThemeData.light(
+      //   scheme: FlexScheme.materialBaseline,
+      //   surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+      //   blendLevel: 7,
+      //   subThemesData: const FlexSubThemesData(
+      //     blendOnLevel: 10,
+      //     blendOnColors: false,
+      //     useTextTheme: true,
+      //     useM2StyleDividerInM3: true,
+      //   ),
+      //   visualDensity: FlexColorScheme.comfortablePlatformDensity,
+      //   useMaterial3: true,
+      //   swapLegacyOnMaterial3: true,
+      //   // To use the Playground font, add GoogleFonts package and uncomment
+      //   // fontFamily: GoogleFonts.notoSans().fontFamily,
+      // ),
+      // darkTheme: FlexThemeData.dark(
+      //   scheme: FlexScheme.materialBaseline,
+      //   surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+      //   blendLevel: 13,
+      //   subThemesData: const FlexSubThemesData(
+      //     blendOnLevel: 20,
+      //     useTextTheme: true,
+      //     useM2StyleDividerInM3: true,
+      //   ),
+      //   visualDensity: FlexColorScheme.comfortablePlatformDensity,
+      //   useMaterial3: true,
+      //   swapLegacyOnMaterial3: true,
+      //   // To use the Playground font, add GoogleFonts package and uncomment
+      //   // fontFamily: GoogleFonts.notoSans().fontFamily,
+      // ),
 // If you do not have a themeMode switch, uncomment this line
 // to let the device system mode control the theme mode:
 // themeMode: ThemeMode.system,
